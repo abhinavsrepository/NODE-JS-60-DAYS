9849e966e7431409143ddee38f2332bd1228e462
Abhinav Singh <95268334+abhinavsrepository@users.noreply.github.com>
2023-10-22 15:21:22 +0530

    22/10

---
+++ functions.js
@@ -0,0 +1,10 @@
+//undestand scope 
+//write hiher order function 
+//pass function as callbacks
+/* scope ==> variable visibilty where we have access to that variable wher we descript the variable
+ */
+//function scope
+function helpme(){
+    let msg = 'i am= happy'
+
+}
