a5018f42fa1ad495aab739a57744806b1eef416d
Abhinav Singh <95268334+abhinavsrepository@users.noreply.github.com>
2023-11-28 19:23:39 +0530

    kk

--- javascript interview/functionscopeandblockscope.js
+++ javascript interview/functionscopeandblockscope.js
@@ -1,2 +1,14 @@
 wht is a scope of a variable ?]
-for eg
+for eg if variable is defined in oone function it will not be used in other function
+
+function a(){
+    let x=10;
+
+}
+
+function b(){
+    console.log(x);
+    x will not be accessed
+    
+
+}
