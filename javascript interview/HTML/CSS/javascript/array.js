549bf3767a1f260de338e0c580f51e69d40e5657
Abhinav Singh <95268334+abhinavsrepository@users.noreply.github.com>
2023-12-05 13:05:46 +0530

    files added

---
+++ javascript interview/HTML/CSS/javascript/array.js
@@ -0,0 +1,10 @@
+var shoppingList =['cereal','cheese','ice'];
+
+// empty array 
+let lotto =[45,23,34,35,45]
+// accessing the array
+
+let colors =['red', 'orange','green'];
+// now accessing the command and changing them
+colors[0]=redis
+let shoppingList= ['chess']
--- javascript interview/functionscopeandblockscope.js
+++ javascript interview/functionscopeandblockscope.js
@@ -1,14 +1,14 @@
-wht is a scope of a variable ?]
-for eg if variable is defined in oone function it will not be used in other function
+// wht is a scope of a variable ?]
+// for eg if variable is defined in oone function it will not be used in other function
 
-function a(){
-    let x=10;
+// function a(){
+//     let x=10;
 
-}
+// }
 
-function b(){
-    console.log(x);
-    x will not be accessed
+// function b(){
+//     console.log(x);
+//     x will not be accessed
     
 
-}
+// }
