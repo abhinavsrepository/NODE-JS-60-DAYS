30d157d242a692b5c272004d708974045bc630fd
Abhinav Singh <95268334+abhinavsrepository@users.noreply.github.com>
2023-12-09 10:56:32 +0530

    javascript topic wise code leaarnig

--- jstopicwise/objects.js
+++ jstopicwise/objects.js
@@ -1,15 +1,31 @@
-const fitbitdata =[
-    30354,
+// const fitbitdata =[
+//     30354,
 
-    1812,
+//     1812,
 
-    2132,
+//     2132,
 
-// here the data entered is make in the ordered format
-]
-const lucyfitbit=[
-    2313124,1234,
-    231321,
-    3423123,
-]
-let lucytotalsteps =121212
+// // here the data entered is make in the ordered format
+// ]
+// const lucyfitbit=[
+//     2313124,1234,
+//     231321,
+//     3423123,
+// ]
+// let lucytotalsteps =121212
+
+
+//An object stores data in th formnat of 
+const chitbitdata ={
+    totalsteps :20202,
+    totalmiles:222.7,
+    avgcalorieburn:5755,
+    workoutthisweek:'50r7time',
+
+    // specify steps 
+    // collection of property 
+    // object is container is key value storage 
+    // 
+
+
+}
