4feca522709f198a27d2bc2e95ac3f70932bdf89
Abhinav Singh <95268334+abhinavsrepository@users.noreply.github.com>
2023-12-09 10:40:00 +0530

    javascript topic wise code leaarnig

---
+++ jstopicwise/objects.js
@@ -0,0 +1,15 @@
+const fitbitdata =[
+    30354,
+
+    1812,
+
+    2132,
+
+// here the data entered is make in the ordered format
+]
+const lucyfitbit=[
+    2313124,1234,
+    231321,
+    3423123,
+]
+let lucytotalsteps =121212
