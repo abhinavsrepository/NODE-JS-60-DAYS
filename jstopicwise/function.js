63120c7a4c90bc544289d1d090d116617cc7f484
Abhinav Singh <95268334+abhinavsrepository@users.noreply.github.com>
2023-12-09 11:51:09 +0530

    html code

---
+++ jstopicwise/function.js
@@ -0,0 +1,25 @@
+// function step to write a function
+// // define==> run
+// // defining a function
+// function name(){
+//     // do something
+//     console.log('javascript is the best')
+
+// }
+
+// for (let i=0 ; i<10;i++){
+//     name()
+// }
+
+// generating dice roll
+function rollDie (){
+let roll=Math.floor(Math.random()*6)+1;
+console.log(`ROlled:${roll}`);
+}
+function throwDice(){
+    rollDie();
+    rollDie();
+
+}
+// function argument 
+ 
