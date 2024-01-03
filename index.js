0de7d4aba1c83962e675f883b32cb2261453781d
Abhinav Singh <95268334+abhinavsrepository@users.noreply.github.com>
2024-01-04 01:54:57 +0530

    04-1-24

--- index.js
+++
@@ -1,5 +0,0 @@
-
-const lib =require ('./lib.js')
-
-console.log(lib.sum(4,5),lib.diff(3,6))
-const a=5;
--- request.js
+++
@@ -1,9 +0,0 @@
-
-const app = express();
-app.get ("/", (req,res)=>{
-    res.send ("hello world!");
-
-});
-app.listen (3000,()=>{
-    console.log("server running on port 3000")
-})
