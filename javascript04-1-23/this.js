8f527083d622febf95046baf41882c31ca7926f5
Abhinav Singh <95268334+abhinavsrepository@users.noreply.github.com>
2024-01-04 17:20:38 +0530

    this keyword added

--- javascript04-1-23/this.js
+++ javascript04-1-23/this.js
@@ -11,4 +11,53 @@ user1.sayHI=function (){
     alert ("hello!");
 
 }
-user1.sayHi();
+
+// this in method common object method needs to access the information stored un the object to do its jobs 
+// acces the mehtod can use the this keyword 
+// the values of this is the object "before dot" the one used to call the method
+
+let user3 ={
+    name :"John",
+    age:30,
+    sayHi(){
+        alert(this.name);// this is the current object
+    }
+};
+user.sayHi();
+//  but code above is unrelaiable because of large program doesnt use it 
+let user4 ={
+    name :"John",
+    age:30,
+    sayHi(){
+        alert (user.name);
+
+    }
+
+
+}
+let admin = user ;
+// user = null ;overwrite to make things obviuous 
+admin.sayHi();
+// this doesntot behave properly like other programming language 
+//  calling without an object : 
+this == undefined
+
+//  arrrow function have no this 
+let users ={
+    firstName : "Ilya",
+    sayHi(){
+        let arrow =() =>alert(this.firstName);
+        arrow();
+    }
+};
+user.sayHi();
+
+// Summary 
+// function that can stored in object
+function makeUser(){
+    return {
+        name:"john",
+        ref:this
+    };
+}
+
