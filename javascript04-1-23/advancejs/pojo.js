a74570f59e960b14e0ab4ea85a63710ab7ca7cd4
Abhinav Singh <95268334+abhinavsrepository@users.noreply.github.com>
2024-01-04 17:01:00 +0530

    this keyword added

--- javascript04-1-23/advancejs/pojo.js
+++ javascript04-1-23/advancejs/pojo.js
@@ -1,4 +1,11 @@
 const key =" species"
 const pet ={species :"DOG",name:"Elton",age:1};
 // stored in string when stores
- 
+//   details you should know
+// propertues tgat does not exists evaluate to undefined:
+o1.taco // undefined 
+// all keys get stringed
+pet[true]= "hello!";
+
+// what is o1[1]?
+o1[1];
---
+++ javascript04-1-23/this.js
@@ -0,0 +1,14 @@
+let user ={
+    name :"john",
+    age:30 
+};
+// method examples 
+let user1 ={
+    name :"john",
+    age:30
+}
+user1.sayHI=function (){
+    alert ("hello!");
+
+}
+user1.sayHi();
