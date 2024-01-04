8626949624f1f7bcef07427a436a6e7b82324663
Abhinav Singh <95268334+abhinavsrepository@users.noreply.github.com>
2024-01-04 13:35:50 +0530

    typescript added

---
+++ TypeScript/intro.ts
@@ -0,0 +1,16 @@
+// // Accessing the property 'toLowerCase'
+// // on 'message' and then calling it
+//  message = "Hello World!";
+// message.toLowerCase();
+// // Calling 'message'
+// message();
+function flipCoin(){
+
+    return Math.random() <0.5;
+}
+//  basic logic errors
+const value = Math.random () < 0.5? "a":"b";
+if (value !== "a"){
+    console.log("hello world")
+}
+   
--- javascript04-1-23/arrowfunction.js
+++ javascript04-1-23/arrowfunction.js
@@ -1,4 +1,19 @@
 // let func =(arg1,arg2,argN) =>expression;
 let func =function(arg1,arg){
     return expression;
+};
+let sum = (a,b) =>a+b;
+alert (sum(1,2));
+
+//normal functiom 
+function showme(){
+    alert ('hello everypone');
+
+}
+
+//  fhuinction calling
+showme()
+
+function name (parameter1,parameter2,parameterN){
+
 }
