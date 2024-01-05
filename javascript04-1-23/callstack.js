535ba500f88600d71350929aac1bc0fe5c30aa83
Abhinav Singh <95268334+abhinavsrepository@users.noreply.github.com>
2024-01-06 00:00:00 +0530

    hell no

--- javascript04-1-23/callstack.js
+++ javascript04-1-23/callstack.js
@@ -8,4 +8,7 @@ function f1() {
   }
   
   f1(); // Output: f1 called, f2 called
-  
+//   
+//  the JavaScript call stack is like a stack of plates representing function calls. Each call adds a plate (stack frame) with function info. When a function finishes, its plate (frame) is removed, returning to the previous function. It helps keep track of nested functions and code execution order.
+
+// Think of it as a temporary "to-do list" for functions in JavaScript. New tasks (functions) go on top, and once finished, they get "done" and removed.
